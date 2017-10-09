a26d1aefc27f1baf0f0e11a4aa52f3472eeeb3f7
Maxime Kehon <mkehon@e1z2r3p45.42.us.org>
2017-10-09 11:03:34 -0700

    update

--- srcs/in_valid.c
+++ srcs/in_valid.c
@@ -6,7 +6,7 @@
 /*   By: mkehon <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/10/06 13:51:39 by mkehon            #+#    #+#             */
-/*   Updated: 2017/10/06 17:14:38 by mkehon           ###   ########.fr       */
+/*   Updated: 2017/10/09 10:59:56 by mkehon           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -68,12 +68,15 @@ int		is_shape(char *str)
 	return (0);
 }  
 
-/* after read, each buffer ends with '\0'*/
+/*
+** after read, each buffer ends with '\0'
+** if read read 0 buf keeps previous value
+*/
 int		main(int argc, char **argv)
 {
-
 	int		fd;
 	char	buf[21];
+	int		temp;
 	int		lu;
 	
 	fd = 0;
@@ -81,17 +84,17 @@ int		main(int argc, char **argv)
 	if (argc == 2)	
 	{
 		fd = open(argv[1], O_RDONLY); // return the address of the file
-		while (lu != 0)
+		while (lu != 0) // lu = nb of char 'open' was able to read. if 0 it is a problem.
 		{
+			temp = lu;
 			lu = read(fd, buf, 21); // read in fd 21 char into buffer
+			if (lu == 0)
+				break;
 			if (!is_grid(buf) || !is_shape(buf))
-			{
-				printf("is_grid: %d | is_shape: %d\n", is_grid(buf), is_shape(buf));
 				return (0);
-			}
 		}
+		if (temp != 20)
+			return (0);
 	}
-
-	printf("Wesh alors\n");
 	return (1);
 }
